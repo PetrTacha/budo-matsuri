6c21d886f48e8eb3d97e5d75a869dc8360871e05
Petr Tácha <tachpe1997@gmail.com>
2024-02-20 22:18:01 +0100

    Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from '@/components/Hero.module.scss';
+import CounterTime from "@/components/CounterTime";
 
 const Hero = () => {
+
+
   return (
-    <section className={`${styles.row} ${styles.greyRow}`}>
+    <section className={`${styles.hero} ${styles.row} ${styles.greyRow}`}>
       <div className="container mx-auto flex items-center justify-center flex-col relative z-20">
-        <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold mb-4 text-center">Welcome to Our Website</h1>
-        <p className="text-lg lg:text-xl mb-8 text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        <button className="bg-white text-blue-500 py-2 px-6 rounded-full shadow-lg hover:bg-blue-400 transition duration-300 ease-in-out">Get Started</button>
+        <h1 className="text-4xl md:text-5xl lg:text-9xl font-bold mb-10 text-center">BUDO MATSURI</h1>
+        <CounterTime />
+        <h2 className="text-lg lg:text-4xl mb-8 text-center">- FESTIVAL JAPONSKÝCH BOJOVÝCH UMĚNÍ -</h2>
       </div>
       <div className={`absolute inset-0 z-0 ${styles.bgOverlay}`}></div> {/* Overlay */}
       {/* <style jsx>{`
