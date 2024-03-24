64d33319eb48ffaa655042c179eb69127eda1178
Petr Tácha <petr.tacha@nextedy.com>
2024-03-24 16:51:30 +0100

    rounded

--- src/components/Modals/ClubModal/ClubModal.jsx
+++ src/components/Modals/ClubModal/ClubModal.jsx
@@ -36,20 +36,20 @@ export const ClubModal = ({ klub, open = false, setOpen }) => {
               leaveFrom="opacity-100 translate-y-0 sm:scale-100"
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
-              <Dialog.Panel className="relative transform rounded-lg bg-white shadow-xl transition-all sm:w-4/6 w-5/6 sm:h-3/4 h-auto flex sm:flex-row flex-col">
+              <Dialog.Panel className="relative transform rounded-lg bg-white shadow-xl transition-all sm:w-4/6 w-5/6 sm:h-3/4 h-5/6 flex sm:flex-row flex-col">
                 {/* <div className="mt-3 text-center sm:mt-5 flex flex-col gap-7 h-full"> */}
                 <span
                   className={`absolute right-2 top-2 cursor-pointer ${styles.crossStandAlone}`}
                   onClick={() => setOpen(false)}
                 ></span>
-                <div className="md:flex-1 overflow-hidden rounded-l-lg">
+                <div className={`flex-none sm:flex-1 overflow-hidden ${styles.rounded}`}>
                   <img
                     src={klub.thumbnail}
                     alt={removeNewSpace(klub.name)}
                     className="object-cover w-full h-full"
                   />
                 </div>
-                <div className="bg-red flex-1 rounded-r-lg">
+                <div className="bg-red flex-none sm:flex-1 rounded-r-lg">
                   <h3 className="flex-1 mt-10">{removeNewSpace(klub.name)}</h3>
 
                   <div className="relative h-4/6">
