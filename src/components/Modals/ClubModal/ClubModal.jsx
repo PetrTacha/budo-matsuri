99d496b618e73ea523e43213f4e33dd146a7c4ae
Petr Tácha <petr.tacha@nextedy.com>
2024-03-29 16:18:55 +0100

    fix fetch

--- src/components/Modals/ClubModal/ClubModal.jsx
+++ src/components/Modals/ClubModal/ClubModal.jsx
@@ -13,7 +13,7 @@ export const ClubModal = ({ klub, open = false, closeModal }) => {
   useEffect(() => {
     if (open) {
       // Assuming the description property contains the path to the HTML file
-      const htmlPath = klub.description;
+      const htmlPath = encodeURIComponent(klub.description);
       fetch(htmlPath)
         .then((response) => {
           if (response.status === 404) {
