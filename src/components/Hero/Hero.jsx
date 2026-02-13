48420a2fcd1fd151aada3578512fcd3928834db8
Petr Tácha <tachpe1997@gmail.com>
2026-02-13 15:42:31 +0100

    feat(HERO): layout, pic fonts etc.

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,37 +7,22 @@ import { ROUTES } from "@/constants";
 const Hero = () => {
   return (
     <BudoRow withImage={true}>
-      <div className="container mx-auto flex items-center justify-start flex-col relative pt-16 md:pb-40 z-10 pb-16">
-        <h1
-          className={`text-6xl md:text-8xl font-bold mb-10 mx-2 text-center ${styles.heroTitle}`}
-        >
-          BUDŌ MATSURI
-        </h1>
-        
-        {/* <h2
-          className={`text-2xl md:text-5xl mb-8 text-center mx-3 font-bold mt-7 md:mt-24 ${styles.heroSubTitle}`}
-        >
-          Děkujeme všem, kteří dorazili na Budō matsuri.
-        </h2>
-        <p className="md:text-4xl text-xl mx-3 text-center">
-          Uvidíme se na dalších slavnostech v roce 2026!
-        </p>
-        <p className="md:text-2xl text-md mx-3 md:mt-20 mt-10 text-center mb-5">
-          Mezitím si můžete prohlédnout fotografie z minulých ročníků.
-        </p>
-        <Button href={ROUTES.GALLERY}>
-          Fotogalerie
-        </Button> */}
-        <h3
-          className={`text-2xl md:text-5xl mb-8 text-center mx-3 mt-7 md:mt-24 ${styles.heroSubTitle}`}
-        >
-          FESTIVAL JAPONSKÝCH BOJOVÝCH UMĚNÍ
+      <div className="container mx-auto flex items-start justify-start flex-col relative pt-72 p-32 md:pb-40 z-10 pb-16">
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img
+          src="/logos/FullLogo.svg"
+          alt="Budō Matsuri Logo"
+          className="w-96 md:w-[32rem] mb-1"
+          loading="eager"
+          decoding="async"
+        />{" "}
+        <h3 className={`mb-32 md:mt-1 ${styles.heroSubTitle}`}>
+          Festival japonských bojových umění
         </h3>
-        <div className="flex md:gap-16 font-normal mx-2 mt-5 flex-col md:flex-row text-center">
-          <div className="md:text-4xl text-xl">28. října 2026</div>
-          <div className="md:text-4xl text-xl font-bold">11:00 - 18:00</div>
-          <div className="md:text-4xl text-xl">Pod Juliskou 4</div>
-        </div>
+        <h2 className="flex  mx-2 mt-5 flex-col">
+          <div className="">28. října 2026, 11:00 - 18:00</div>
+          <div className="">Pod Juliskou 4</div>
+        </h2>
       </div>
     </BudoRow>
   );
