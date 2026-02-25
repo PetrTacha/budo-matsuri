7aebd575e86df042c707f1555b1bca61d9c1abcf
Petr Tácha <tachpe1997@gmail.com>
2026-02-25 14:33:04 +0100

    feat(circle): small large

--- src/components/common/Circle/Circle.jsx
+++ src/components/common/Circle/Circle.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import styles from "./Circle.module.scss";
 
-const Circle = ({ h2Text, h3Text = "", position = "relative", className = "" }) => {
+const Circle = ({ h2Text, h3Text = "", position = "relative", className = "", size = "large" }) => {
   const positionClass = position === "absolute" ? styles.absolute : styles.relative;
+  const sizeClass = size === "small" ? styles.small : styles.large;
 
   return (
     <div
-      className={`${styles.circle} ${positionClass} items-center justify-center flex flex-col text-center ${className}`}
+      className={`${styles.circle} ${sizeClass} ${positionClass} items-center justify-center flex flex-col text-center ${className}`}
     >
       <h2 className={`text-4xl font-bold text-white ${styles.circleText}`}>
         {h2Text}
