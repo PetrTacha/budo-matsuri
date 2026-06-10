e59bc0565d650b3c3dfaf1ecf014858fb5f3a07e
Petr Tácha <tachpe1997@gmail.com>
2026-06-10 20:52:46 +0200

    feat(icon): add favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,6 +10,12 @@ export default function RootLayout({ children }) {
     <html lang="cs">
       <head>
         <link rel="stylesheet" href="https://use.typekit.net/iyu1hws.css" />
+        <link
+          rel="icon"
+          href="/icon?<generated>"
+          type="image/<generated>"
+          sizes="<generated>"
+        />
       </head>
       <body>{children}</body>
     </html>
