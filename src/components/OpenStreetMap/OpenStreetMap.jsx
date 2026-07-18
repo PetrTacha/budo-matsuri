25673d98884da5082f3e26908a0201154df537c9
Petr Tácha <tachpe1997@gmail.com>
2026-07-18 21:41:58 +0200

    feat(map): fix and features

--- src/components/OpenStreetMap/OpenStreetMap.jsx
+++ src/components/OpenStreetMap/OpenStreetMap.jsx
@@ -11,12 +11,53 @@ import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 import styles from "./OpenStreetMap.module.scss";
 
-const icon = L.icon({ iconUrl: "/icons/marker_here.svg", iconSize: [50, 50] });
+const icon = L.icon({
+  iconUrl: "/icons/marker_here.svg",
+  iconSize: [50, 50],
+  iconAnchor: [25, 50],
+  popupAnchor: [0, -50]
+});
+
+/**
+ * @param {string} iconUrl
+ */
+const createTransportIcon = (iconUrl) =>
+  L.icon({
+    iconUrl,
+    iconSize: [22, 22],
+    iconAnchor: [11, 11],
+    popupAnchor: [0, -11],
+    className: styles.transportIcon
+  });
+
+const transportStops = [
+  {
+    id: "metro-dejvicka",
+    position: [50.1009, 14.3922],
+    iconUrl: "/icons/map_icons/metro.png",
+    title: "Metro A",
+    subtitle: "stanice Dejvicka"
+  },
+  {
+    id: "podbaba",
+    position: [50.1117631, 14.3940572],
+    iconUrl: "/icons/map_icons/tram.png",
+    title: "Nadrazi Podbaba",
+    subtitle: "Tramvaj 8; Autobus 340, 355"
+  },
+  {
+    id: "bus-cinska",
+    position: [50.1088792, 14.3940811],
+    iconUrl: "/icons/map_icons/bus.png",
+    title: "Autobus 107, 147",
+    subtitle: "stanice Cinska"
+  }
+];
 
 const OpenStreetMap = () => {
-  const [center, setCenter] = useState({ lat: 50.1105, lng: 14.3907 });
+  const [center] = useState({ lat: 50.1105, lng: 14.3907 });
   const ZOOM_LEVEL = 17;
-  const mapRef = useRef();
+  const mapRef = useRef(null);
 
   return (
     <div className={styles.mapContainer}>
@@ -36,8 +77,22 @@ const OpenStreetMap = () => {
           position={[center.lat, center.lng]}
           icon={icon}
         >
-          <Popup><b>Budo Matsuri</b><br />Sportovní areál ČVUT<br />Pod Juliskou 4, Praha 6</Popup>
+          <Popup><b>Budō Matsuri</b><br />Sportovní areál ČVUT<br />Pod Juliskou 4, Praha 6</Popup>
         </Marker>
+
+        {transportStops.map((stop) => (
+          <Marker
+            key={stop.id}
+            position={[stop.position[0], stop.position[1]]}
+            icon={createTransportIcon(stop.iconUrl)}
+          >
+            <Popup>
+              <b>{stop.title}</b>
+              <br />
+              {stop.subtitle}
+            </Popup>
+          </Marker>
+        ))}
       </MapContainer>
     </div>
   );
@@ -80,6 +135,6 @@ export default OpenStreetMap;
 
   L.marker(venue, { icon: icon })
     .addTo(map)
-    .bindPopup("<b>Budo Matsuri</b><br>Sportovní areál ČVUT<br>Pod Juliskou 4, Praha 6")
+    .bindPopup("<b>Budō Matsuri</b><br>Sportovní areál ČVUT<br>Pod Juliskou 4, Praha 6")
     .openPopup();
 </script> */}
